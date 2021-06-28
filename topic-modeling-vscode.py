0a425bb04c58b6130414112512131a02a0b03f8d
pellucidots <86381759+pellucidots@users.noreply.github.com>
2021-06-28 14:37:05 -0400

    Update topic-modeling-vscode.py

--- topic-modeling-vscode.py
+++ topic-modeling-vscode.py
@@ -8,17 +8,22 @@ inCorpus = "issues" #define inCorpus as issues
 outCorpus = "60s-tm" #define outCorpus as the decade you want 
 
 if os.path.exists(outCorpus) == False: #use comparison operator to text the existence of the folder
-    os.mkdir(outCorpus) #creates outCorpus (come back to 10 & 11 later)
+    os.mkdir(outCorpus) #create outCorpus (come back to 10 & 11 later)
 
 filenames = [] #create a list
-for root, dirs, files, in os.walk(inCorpus, topdown=False): #root 
-        if '196' in name:
-            filenames.append(os.path.join(root, name))
+for root, dirs, files, in os.walk(inCorpus, topdown=False): 
+# root : prints out directories only from what you specified (this case root is inCorpus)
+# dirs : prints out sub-directories from root
+# files : prints out all files from root and directories
 
-for issue in filenames:
-    f = open(issue, 'r', encoding='utf-8')
-    text = f.read()
-    f.close()
+    for name in files: #create for loop iterating through files
+        if '196' in name: #search for files that start with 196
+            filenames.append(os.path.join(root, name)) #add issues and 196 to list of files
+
+for issue in filenames: #create for loop iterating through every issue
+    f = open(issue, 'r', encoding='utf-8') #open and read the issue
+    text = f.read() #set text equal to the issue that is read
+    f.close() #
     characters = len(text)
     chunks = math.floor(characters/500)
  # print(chunks)
