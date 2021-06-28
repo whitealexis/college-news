bfc066a50aedf6bf9f7535103a7bc6e8115f6823
pellucidots <86381759+pellucidots@users.noreply.github.com>
2021-06-28 14:58:24 -0400

    Update topic-modeling-vscode.py

--- topic-modeling-vscode.py
+++ topic-modeling-vscode.py
@@ -12,9 +12,9 @@ if os.path.exists(outCorpus) == False: #use comparison operator to text the exis
 
 filenames = [] #create a list
 for root, dirs, files, in os.walk(inCorpus, topdown=False): 
-# root : prints out directories only from what you specified (this case root is inCorpus)
-# dirs : prints out sub-directories from root
-# files : prints out all files from root and directories
+# root: prints out directories only from what you specified (this case root is inCorpus)
+# dirs: prints out sub-directories from root
+# files: prints out all files from root and directories
 
     for name in files: #create for loop iterating through files
         if '196' in name: #search for files that start with 196
@@ -23,23 +23,26 @@ for root, dirs, files, in os.walk(inCorpus, topdown=False):
 for issue in filenames: #create for loop iterating through every issue
     f = open(issue, 'r', encoding='utf-8') #open and read the issue
     text = f.read() #set text equal to the issue that is read
-    f.close() #
-    characters = len(text)
-    chunks = math.floor(characters/500)
- # print(chunks)
+    f.close() #close text file so it cannot be read or written. important to do to prevent data loss
+    characters = len(text) #define characters as the entire length of the text
+    chunks = math.floor(characters/500) 
+  #chunks: defined as each 500 character chunk
+  #math.floor: rounds the number down
+    
+# print(chunks)
 # range(chunks)
-    issue_name = issue[7:19]
-    start=0
-    stop=500
+    issue_name = issue[7:19] #issue_name defined as characters 7 through 19 in the file name, extracting the date (YYYY, MM, DD)
+    start=0 #start at character 0
+    stop=500 #stop at character 500
 
-    for i in range(chunks):
-        slice = text[start:stop]
-        start+=500
-        stop+=500
-        newfile = outCorpus + "/" + issue_name + '-chunk' + str(i) + '.txt'
+    for i in range(chunks): #come back
+        slice = text[start:stop] #create slices in text at the start and stop marks indicated above
+        start+=500 #start+ iterates through each successive set of 500 character chunks
+        stop+=500 #stop+ tells loop to stop and restart at the end of the 500 characters
+        newfile = outCorpus + "/" + issue_name + '-chunk' + str(i) + '.txt' #create new file name
 
-f = open(newfile, "w")
-f.write(slice)
+f = open(newfile, "w") #open and write new chunked files
+f.write(slice) #
 f.close()
 
 
