e0f400d51393df1c97574bc07686b57ed9cb035b
Alice T McGrath <amcgrath1@brynmawr.edu>
2021-07-12 12:22:57 -0400

    remove decorative strings, edit functions

--- corpus-cleaning/text-cleaning.py
+++ corpus-cleaning/text-cleaning.py
@@ -20,6 +20,11 @@ start_line = re.compile(r'^([^a-zA-Z]*)(([a-z]|[A-Z])+)') # identify the first w
 english_vocab = set(w.lower() for w in nltk.corpus.words.words()) # create a set of English words using NLTK
 wordnet_lemmatizer = nltk.WordNetLemmatizer() # create a word net lemmatizer
 
+def clean_words(text): # removes zeroes, accents and non-word strings
+    no_accents = unidecode.unidecode(text) # remove accents from accented characters 
+    no_decor = re.sub(r'\w*(e{3,}|c{3,})\w*', '', no_accents) #gets rid of misrecognized decorations
+    no_zeroes = re.sub(r'([a-z]+)0([a-z]+)', r'\1o\2', no_decor) # if there is a zero in the middle of a word, replace it with an "o"
+    return no_zeroes # return the string
 
 def fix_hyphens(text): # for a given string, find hyphens at the end of a line and try to join together words that have been split
     lines = text.split("\n") # split into lines using the newline character
@@ -43,11 +48,6 @@ def fix_hyphens(text): # for a given string, find hyphens at the end of a line a
                     linesclean[i] = c[:c.index(target.group(0))] # remove the beginning of the word from the current line
     output = "\n".join(linesclean) # make the list into a string separated by new line characters
     return output # return the string
-                    
-def clean_words(text): # remove symbols and zeros from words
-    no_zeroes = re.sub(r'([a-z]+)0([a-z]+)', r'\1o\2', text) # if there is a zero in the middle of a word, replace it with an "o"
-    no_symbols = re.sub(r'([a-z]+)[|"({})]([a-z]+)', r'\1\2', no_zeroes) # replace any non-alphabetical characters in the middle of word
-    return no_symbols # return the string
 
 def remove_symbols(text): # remove symbols from a string
     no_symbols = re.sub(r'[^a-z]', " ", text) # replace any non-alphabetical characters with a space
@@ -61,8 +61,8 @@ def lemmatize(tokens): # lemmatize each word in a list
     return lemmatized # return the list of lemmatized words
     
 def deep_clean_list(text): # clean the text by running the following steps:
-    text_unaccented = unidecode.unidecode(text) # remove accents from accented characters
-    no_hyphens = fix_hyphens(text_unaccented) # fix hyphens at the end of lines
+    ocr_cleaner = clean_words(text) # remove zeros, non-word strings, accents
+    no_hyphens = fix_hyphens(ocr_cleaner) # fix hyphens at the end of lines
     text_lower = no_hyphens.lower() # make the whole text lowercase 
     no_symbols = remove_symbols(text_lower) # remove symbols 
     words = nltk.word_tokenize(no_symbols) # split into words 
@@ -70,12 +70,12 @@ def deep_clean_list(text): # clean the text by running the following steps:
     return lemmatized # return the cleaned, lemmatized text as a list of words
 
 def clean(text):
-    text_unaccented = unidecode.unidecode(text) # remove accents from accented characters
-    no_hyphens = fix_hyphens(text_unaccented) # fix hyphens at the end of lines 
+    ocr_cleaner = clean_words(text)
+    no_hyphens = fix_hyphens(ocr_cleaner) # fix hyphens at the end of lines 
     no_symbols = clean_words(no_hyphens) # remove zeros & symbols
     return no_symbols # return the cleaned version of the text
 
-infile = 'issues/cn1914-11-12.txt' # filepath for input text
+infile = 'issues/cn1959-10-07.txt' # filepath for input text
 with open(infile, 'r', encoding='utf-8') as f: # open the file
     text = f.read() # read the file
 
