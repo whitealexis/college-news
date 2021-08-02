5d11f47152141bcceeb92a73ade700ac5db27229
Alice T McGrath <amcgrath1@brynmawr.edu>
2021-08-02 13:02:50 -0400

    organize and update issue length viz to fix date problem

--- issue-length-viz/scatter_issue_length.py
+++ issue-length-viz/scatter_issue_length.py
@@ -15,14 +15,17 @@ import altair as alt
 
 ############### Reading a csv into a data frame ##############
 
-csvpath = "/Users/amcgrath1/college-news/repo-code/college-news/data/cn-metadata-wordcounts.csv"
-df = pd.read_csv(csvpath) # creates a dataframe from the csv file
-df.head(20) # preview the first 20 rows of your data frame
-
+csvpath = "data/cn-metadata-wordcounts.csv"
+df = pd.read_csv(csvpath, # creates a dataframe from the csv file
+                parse_dates=['date']) # parses 'date' column as datetime pandas object
+#df.head(20) # preview the first 20 rows of your data frame
+# create a new column with dates in EST timezone
+df['date_est'] = df['date'].dt.tz_localize('EST')
+#df.info() # make sure your columns are the data types you need
 
 ################# Configuring the chart #################
 
-chart = alt.Chart(df # creates 'chart' variable from data frame
+issue_length_chart = alt.Chart(df # creates 'chart' variable from data frame
 ).transform_calculate( # creates a new column by adding the base url to the id number
     url='https://digitalcollections.tricolib.brynmawr.edu/object/bmc'+ alt.datum.id 
 ).mark_point().encode( # scatterplot chart with points
@@ -30,12 +33,12 @@ chart = alt.Chart(df # creates 'chart' variable from data frame
         'words', # selects data frame column 'words'
         title="Word count"), # label text for axis
     alt.X( # sets x axis
-        'yearmonth(date):T', # selects a temporal format (yearmonth), a column (date)
+        'yearmonthdate(date_est):T', # selects a temporal format (yearmonth), a column (date)
         title="Issue date"), # label text for x axis
     alt.Size('pages', title='Number of pages'), # sets mark size to depend on page count
     href='url:N', # makes each point link to the url created above
     tooltip = [ # add a tooltip to show for each point
-        alt.Tooltip('date:T', title="Issue date"), # selects date column & specifies that it's temporal data (T)
+        alt.Tooltip('yearmonthdate(date_est):T', title="Issue date"), # selects date column & specifies that it's temporal data (T)
         alt.Tooltip('vol', title="Vol."), # selects each data frame column to display and how it should be labeled
         alt.Tooltip('no', title="No."),
         alt.Tooltip('pages', title="Pages"),
@@ -54,9 +57,9 @@ chart = alt.Chart(df # creates 'chart' variable from data frame
 
 ################# Saving the chart as html #################
 
-chart # displays chart in interactive viewer (doesn't work from command line)
+issue_length_chart # displays chart in interactive viewer (doesn't work from command line)
 
-chart.save('issue_length.html')
+issue_length_chart.save('issue-length-viz/issue_length.html')
 
 # To save chart in other formats (png, pdf) install altair_saver package
 # See https://altair-viz.github.io/user_guide/saving_charts.html
